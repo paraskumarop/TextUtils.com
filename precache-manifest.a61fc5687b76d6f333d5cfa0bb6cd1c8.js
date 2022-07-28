93baa6ccdd6ecd24503a8c2f2a4a3f8d3f2ac052
Paras <paraskumarop@gmail.com>
2022-07-28 14:26:22 +0530

    Updates

---
+++ precache-manifest.a61fc5687b76d6f333d5cfa0bb6cd1c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "93e5727e15e7240ffd43",
+    "url": "/TextUtils.com/static/css/main.6b9842eb.chunk.css"
+  },
+  {
+    "revision": "93e5727e15e7240ffd43",
+    "url": "/TextUtils.com/static/js/main.93e5727e.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/TextUtils.com/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "2fe1f0e9e98119ee8343",
+    "url": "/TextUtils.com/static/js/2.2fe1f0e9.chunk.js"
+  },
+  {
+    "revision": "a724232a7983104beb3a",
+    "url": "/TextUtils.com/static/js/runtime~main.a724232a.js"
+  },
+  {
+    "revision": "0cdfdf3b48bbdac7838236856eb6d015",
+    "url": "/TextUtils.com/static/media/copy-solid.0cdfdf3b.svg"
+  },
+  {
+    "revision": "816b9292ee4f08b2260338936d0787e5",
+    "url": "/TextUtils.com/index.html"
+  }
+];
